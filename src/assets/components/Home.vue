7574e3d0fdc1d31c2886848fc5ebe1131fb122c5
Sky Blue <dawadiaakash@gmail.com>
2024-08-31 23:19:33 +0545

    Fetch(Form): Get input value on console

--- src/assets/components/Home.vue
+++ src/assets/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
    <H1> Two way Binding:</H1>
-   <input type="text" v-model="count"/>
-   <h3> {{ count }}</h3>
+   <input type="text" placeholder="Enter email" v-model="email" />
+   <br/>
+   <input type="password" placeholder="enter password" v-model="password"/>
+   <br>
+   <button v-on:click="getData()" type="button">Get Values</button>
+
 </template>
 
 
@@ -9,23 +13,19 @@
 export default {
         name: "Home",
         data(){
-            return {
-                count:0
-                }
-            },
-            methods: {
-                getName(name){
-                    return name;
-                
-            
-            },
-            getData(){
-                return {
-                    name: "Cole",
-                    email:this.email
-                }
+            return{
+                email:null,
+                password:null,
+
             }
-            }}
+        },
+        methods: {
+            getData(){
+                console.warn("values:",this.email,this.password)
+
+            }
+        }
+}
     
 
 </script>

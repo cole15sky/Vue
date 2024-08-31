6ad808587a5238360cce16baa4935680b1f64b87
Sky Blue <dawadiaakash@gmail.com>
2024-08-31 22:21:29 +0545

    Fetch(interpolation): Vue interpolation and data binding

---
+++ src/assets/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1> Home Component </h1>
+    <h1> Mobile: {{ mobile }}</h1>
+    <h1>  {{ getName() }}</h1>
+</template>
+
+
+<script>
+export default {
+        name: "Home",
+        data(){
+            return {
+                email:'aakash@gmail.com',
+                mobile: 12345,
+                getName: function()
+                {
+                    return "Madoona: True Blue"
+                }
+
+                }
+            }
+        }
+    
+
+</script>
+
+<style scoped>
+
+</style>
